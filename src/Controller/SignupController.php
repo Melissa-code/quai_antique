22af7c0b27e7beb28d6302cb89a10a12cfb47dfd
Melissa Benard <benardmeli@yahoo.fr>
2023-02-14 17:30:57 +0100

    sign-up page

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\SignupType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SignupController extends AbstractController
 {
-    #[Route('/signup', name: 'app_signup')]
-    public function index(): Response
+    #[Route('/inscription', name: 'app_signup')]
+    public function signup(Request $request): Response
     {
-        return $this->render('signup/index.html.twig', [
-            'controller_name' => 'SignupController',
+        $user = new User();
+        $form = $this->createForm(SignupType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+        }
+
+
+        return $this->render('signup/signup.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/SignupType.php
+++ src/Form/SignupType.php
@@ -2,23 +2,101 @@
 
 namespace App\Form;
 
+use App\Entity\Allergy;
+use App\Entity\Guest;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class SignupType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('email')
-            ->add('password')
+            ->add('firstname', TextType::class, [
+                'label'=> 'Prénom * : ',
+                'attr' => [
+                    'placeholder'=> 'Ex: Guillaume'
+                ],
+                'constraints' => new Length(['min'=> 3, 'max'=> 50]),
+                'required' => true
+            ])
+            ->add('lastname', TextType::class, [
+                'label'=> 'Nom * :',
+                'attr' => [
+                    'placeholder'=> 'Ex: Dupont'
+                ],
+                'constraints' => new Length(['min'=> 3, 'max'=> 50]),
+                'required' => true
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email * : ',
+                'attr' => [
+                    'placeholder' => 'Ex: gdupont@hotmail.com'
+                ],
+                'constraints' => new Length(['min'=> 5, 'max'=> 100]),
+                'required' => true
+            ])
+//            ->add('guest', EntityType::class, [
+//                'class' => Guest::class,
+//                //'choice_label' => 'quantity',
+//                'label' => 'Convives :',
+//                'attr' => [
+//                    'placeholder' => 'Ex: 2'
+//                ]
+//            ])
+            ->add('guest', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '4' => 3,
+                ],
+            ])
+//            ->add('allergies', EntityType::class, [
+//                'class' => Allergy::class,
+//                'choice_label' => 'title',
+//                'multiple' => true,
+//                'label' => 'Allergies :',
+//                'attr' => [
+//                    'placeholder' => 'Ex: crustacés'
+//                ]
+//            ])
+            ->add('allergies', ChoiceType::class, [
+                'choices' => [
+                    'crustacés' => 1,
+                    'gluten' => 2,
+                    'arachides' => 3,
+                ],
+            ])
+
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et sa confirmation doivent être identiques',
+                'constraints' => new Length(['min'=> 5, 'max'=> 100]),
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe * : ',
+                    'attr' => [
+                        'placeholder'=> 'Ex: *************'
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe * :',
+                    'attr' => [
+                        'placeholder'=> 'Ex: *************'
+                    ],
+                ]
+            ])
+
             // ->add('roles')
-            ->add('allergies')
-            ->add('guest')
         ;
     }
 
