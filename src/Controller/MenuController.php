a481e11de072dce5ebec12e28a21da55f22aecb9
Melissa Benard <benardmeli@yahoo.fr>
2023-02-28 17:02:38 +0100

    Display each setmenu inside each menu section using the setmenu & menu tables.

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Repository\DaytimeRepository;
 use App\Repository\MenuRepository;
+use App\Repository\SetmenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +13,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menus', name: 'app_menus')]
-    public function menu(DaytimeRepository $daytimeRepository, MenuRepository $menuRepository): Response
+    public function menu(DaytimeRepository $daytimeRepository, MenuRepository $menuRepository, SetmenuRepository $setmenuRepository): Response
     {
         $menus = $menuRepository->findAll();
         $daytimes = $daytimeRepository->findAll();
+        $setmenus = $setmenuRepository->findAll();
+
+
 
         return $this->render('menu/menus.html.twig', [
             'menus' => $menus,
             'daytimes'=> $daytimes,
+            'setmenus' => $setmenus,
+
 
         ]);
     }
