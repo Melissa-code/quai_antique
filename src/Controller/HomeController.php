7cddc356777daded12be17188f095930864f9cf6
Melissa Benard <benardmeli@yahoo.fr>
2023-05-16 14:28:59 +0200

    Redirect the user on the home page with the HTTPS protocole after a logout on Heroku.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,18 +6,28 @@ use App\Repository\DishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * Get and display the favorite dishes in the photos gallery of the home page
      * @param DishRepository $dishRepository
+     * @param Request $request
      * @return Response
      */
     #[Route('/', name: 'app_home')]
-    public function home(DishRepository $dishRepository): Response
+    public function home(DishRepository $dishRepository, Request $request): Response
     {
         $favoriteDishes = $dishRepository->findFavoriteDishes(6);
+
+        // Redirect to the HTTPS protocol of Heroku if it is http
+        $word = "herokuapp.com";
+        $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        if(strpos($url, $word)) {
+            header('Location:https://le-quai-antique-restaurant.herokuapp.com/');
+            exit();
+        }
 
         return $this->render('home/home.html.twig', [
             'favoriteDishes' => $favoriteDishes,
