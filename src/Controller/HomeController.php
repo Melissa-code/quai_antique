de5f296f807127b01e8fa263a27e8f241b39207b
Melissa Benard <benardmeli@yahoo.fr>
2023-05-18 13:44:36 +0200

    Change the file of the logo in the home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,16 +19,6 @@ class HomeController extends AbstractController
     {
         $favoriteDishes = $dishRepository->findFavoriteDishes(6);
 
-        // Redirect to the HTTPS protocol of Heroku if it is http
-        // $word = "herokuapp.com";
-        // $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-        //if(strpos($url, $word)) {
-            //header('Location:https://le-quai-antique-restaurant.herokuapp.com/');
-            //exit();
-            //echo "<script> window.location ='https://le-quai-antique-restaurant.herokuapp.com/'</script>";
-            //return $this->redirect('https://le-quai-antique-restaurant.herokuapp.com/');
-        //}
-
         return $this->render('home/home.html.twig', [
             'favoriteDishes' => $favoriteDishes,
             'admin' => false,
